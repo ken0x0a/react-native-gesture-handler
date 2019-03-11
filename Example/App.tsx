46bd89e2b13110e6f3ba7ae91913ceec4c87cc4b
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-03-11 22:58:47 +0900

    --ts

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import { Text, View, FlatList, StyleSheet, YellowBox } from 'react-native';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import * as React from 'react';
+import { FlatList, StyleSheet, Text, View, YellowBox } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
-
-import SwipeableTable from './swipeable';
-import Rows from './rows';
-import Multitap from './multitap';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import BottomSheet from './bottomSheet';
+import Bouncing from './bouncing';
+import ChatHeads from './chatHeads';
+// import { ComboWithGHScroll, ComboWithRNScroll } from "./combo";
+import doubleDraggable from './doubleDraggable';
+import doubleScalePinchAndRotate from './doubleScalePinchAndRotate';
 import Draggable from './draggable';
-import ScaleAndRotate from './scaleAndRotate';
-import PagerAndDrawer from './pagerAndDrawer';
+import Fling from './fling';
+import forceTouch from './forcetouch';
+import HorizontalDrawer from './horizontalDrawer';
+import Multitap from './multitap';
+// import PagerAndDrawer from './pagerAndDrawer';
 import PanAndScroll from './panAndScroll';
 import PanResponder from './panResponder';
-import Bouncing from './bouncing';
-import HorizontalDrawer from './horizontalDrawer';
-import Fling from './fling/index';
-import doubleDraggable from './doubleDraggable';
-import ChatHeads from './chatHeads';
-import { ComboWithGHScroll, ComboWithRNScroll } from './combo';
-import BottomSheet from './bottomSheet/index';
-import doubleScalePinchAndRotate from './doubleScalePinchAndRotate';
-import forceTouch from './forcetouch';
-import { TouchablesIndex, TouchableExample } from './touchables';
+import Rows from './rows';
+import ScaleAndRotate from './scaleAndRotate';
+import SwipeableTable from './swipeable';
+import { TouchableExample, TouchablesIndex } from './touchables';
 
-YellowBox.ignoreWarnings([
-  'Warning: isMounted(...) is deprecated',
-  'Module RCTImageLoader',
-]);
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 // refers to bug in React Navigation which should be fixed soon
 // https://github.com/react-navigation/react-navigation/issues/3956
 
@@ -36,24 +32,24 @@ const SCREENS = {
   ScaleAndRotate: { screen: ScaleAndRotate, title: 'Scale, rotate & tilt' },
   ScaleAndRotateSimultaneously: {
     screen: doubleScalePinchAndRotate,
-    title: 'Scale, rotate & tilt & more',
+    title: 'Scale, rotate & tilt & more'
   },
-  PagerAndDrawer: { screen: PagerAndDrawer, title: 'Android pager & drawer' },
+  // PagerAndDrawer: { screen: PagerAndDrawer, title: 'Android pager & drawer' },
   HorizontalDrawer: {
     screen: HorizontalDrawer,
-    title: 'Gesture handler based DrawerLayout',
+    title: 'Gesture handler based DrawerLayout'
   },
   SwipeableTable: {
     screen: SwipeableTable,
-    title: 'Gesture handler based SwipeableRow',
+    title: 'Gesture handler based SwipeableRow'
   },
   PanAndScroll: {
     screen: PanAndScroll,
-    title: 'Horizontal pan or tap in ScrollView',
+    title: 'Horizontal pan or tap in ScrollView'
   },
   Fling: {
     screen: Fling,
-    title: 'Flinghandler',
+    title: 'Flinghandler'
   },
   PanResponder: { screen: PanResponder },
   Bouncing: { screen: Bouncing, title: 'Twist & bounce back animation' },
@@ -61,32 +57,32 @@ const SCREENS = {
   //   screen: ChatHeads,
   //   title: 'Chat Heads (no native animated support yet)',
   // },
-  Combo: { screen: ComboWithGHScroll },
+  // Combo: { screen: ComboWithGHScroll },
   BottomSheet: {
     title: 'BottomSheet gestures interactions',
-    screen: BottomSheet,
+    screen: BottomSheet
   },
-  ComboWithRNScroll: {
-    screen: ComboWithRNScroll,
-    title: "Combo with RN's ScrollView",
-  },
+  // ComboWithRNScroll: {
+  //   screen: ComboWithRNScroll,
+  //   title: "Combo with RN's ScrollView"
+  // },
   doubleDraggable: {
     screen: doubleDraggable,
-    title: 'Two handlers simultaneously',
+    title: 'Two handlers simultaneously'
   },
   touchables: {
     screen: TouchablesIndex,
-    title: 'Touchables',
+    title: 'Touchables'
   },
   forceTouch: {
     screen: forceTouch,
-    title: 'Force touch',
-  },
+    title: 'Force touch'
+  }
 };
 
 class MainScreen extends React.Component {
   static navigationOptions = {
-    title: '✌️ Gesture Handler Demo',
+    title: '✌️ Gesture Handler Demo'
   };
   render() {
     const data = Object.keys(SCREENS).map(key => ({ key }));
@@ -96,10 +92,7 @@ class MainScreen extends React.Component {
         data={data}
         ItemSeparatorComponent={ItemSeparator}
         renderItem={props => (
-          <MainScreenItem
-            {...props}
-            onPressItem={({ key }) => this.props.navigation.navigate(key)}
-          />
+          <MainScreenItem {...props} onPressItem={({ key }) => this.props.navigation.navigate(key)} />
         )}
         renderScrollComponent={props => <ScrollView {...props} />}
       />
@@ -127,24 +120,24 @@ const ExampleApp = createStackNavigator(
     ...SCREENS,
     TouchableExample: {
       screen: TouchableExample,
-      title: 'Touchables',
-    },
+      title: 'Touchables'
+    }
   },
   {
-    initialRouteName: 'Main',
+    initialRouteName: 'Main'
   }
 );
 
 const styles = StyleSheet.create({
   list: {
-    backgroundColor: '#EFEFF4',
+    backgroundColor: '#EFEFF4'
   },
   separator: {
     height: 1,
-    backgroundColor: '#DBDBE0',
+    backgroundColor: '#DBDBE0'
   },
   buttonText: {
-    backgroundColor: 'transparent',
+    backgroundColor: 'transparent'
   },
   button: {
     flex: 1,
@@ -152,8 +145,8 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#fff',
-  },
+    backgroundColor: '#fff'
+  }
 });
 
 export default createAppContainer(ExampleApp);
